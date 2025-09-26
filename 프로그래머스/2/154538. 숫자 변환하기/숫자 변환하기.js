ad74033ae31301c74deb52c338f55046be1f9a82
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-09-26 22:28:33 +0900

    [level 2] Title: 숫자 변환하기, Time: 118.78 ms, Memory: 43.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -0,0 +1,26 @@
+function solution(x, y, n) {
+    var answer = 0;
+    
+    const dp = Array.from({length : y+1}, () => 1000000)
+    dp[x] = 0
+    
+    
+    
+    for(let i=x; i <= y; i++){
+        if(i*2 <= y){
+            dp[i*2] = Math.min(dp[i*2], dp[i] + 1)
+        }        
+        
+        if(i*3 <= y){
+            dp[i*3] = Math.min(dp[i*3], dp[i]+1)
+        }
+        
+        if(i+n <= y){
+            dp[i+n] = Math.min(dp[i+n], dp[i] +1)
+        }
+    }
+    
+
+
+    return dp[y] === 1000000 ? -1 : dp[y];
+}
