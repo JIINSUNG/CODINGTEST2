c3cd83ec0b9962cc2b67c3425740a29c7f5b4484
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-10-02 10:22:30 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -0,0 +1,12 @@
+function solution(my_string, n) {
+    var answer = '';
+    
+    for(const char of my_string){
+        for(let i=0; i<n; i++){
+            answer += char
+        }
+    }
+    
+    
+    return answer;
+}
