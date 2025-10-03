748936df0d19bd42c923c9cc9f11a401599da9ca
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-10-03 19:12:43 +0900

    [level 0] Title: 카운트 업, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.js
@@ -0,0 +1,8 @@
+function solution(start_num, end_num) {
+    var answer = [];
+    
+    
+    
+    
+    return Array.from({length: end_num - start_num+1}, (_, num) => num+start_num, start_num)
+}
