a4892596d2bedf1620218af3d0714b9ae5f7d819
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-10-09 11:15:50 +0900

    [level 0] Title: l로 만들기, Time: 31.57 ms, Memory: 39 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.js
@@ -0,0 +1,11 @@
+function solution(myString) {
+    var answer = '';
+    myString = myString.split('')
+    
+    for(let i=0; i< myString.length; i++){
+        if (myString[i] < 'l'){
+            myString[i] = 'l'
+        }
+    }
+    return myString.join('');
+}
