0cb15941e91eb7cee2b4fe962a929f5f856858bd
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-10-12 08:21:31 +0900

    [level 0] Title: 0 떼기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.js
@@ -0,0 +1,12 @@
+function solution(n_str) {
+    var answer = '';
+    
+    while (true) {
+        if (n_str.at(0) == '0'){
+            n_str = n_str.substring(1)
+        }else{
+            break
+        }
+    }
+    return n_str;
+}
