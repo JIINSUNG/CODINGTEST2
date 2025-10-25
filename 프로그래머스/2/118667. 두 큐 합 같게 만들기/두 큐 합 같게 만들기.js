b1d9045e32010571268642b39f2cbfbc590733bb
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-10-25 12:56:12 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 199.55 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -0,0 +1,34 @@
+function solution(queue1, queue2) {
+    var answer = 0;
+    
+    const extendedArr = [...queue1, ...queue2, ...queue1]
+    
+    
+    let i =0
+    let j = queue1.length
+    
+    let currentSum = queue1.reduce((acc, val) => acc+val)
+    
+    let totalSum = currentSum + queue2.reduce((acc, val) => acc+val)
+    
+    const targetSum = totalSum/2
+    
+    while (i < extendedArr.length && j < extendedArr.length){
+        if (currentSum === targetSum){
+            return answer
+        }
+        else if (currentSum < targetSum){
+            currentSum += extendedArr[j++]
+            answer += 1
+        } else if (currentSum > targetSum){
+            currentSum -= extendedArr[i++]
+            answer += 1
+        }
+    }
+    
+    
+    
+    
+    
+    return -1;
+}
